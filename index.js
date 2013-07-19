9d68ebc7b59c6dfb6ba0944a248ed1924d19913b
Max Ogden <max@maxogden.com>
2013-07-19 14:45:14 -0700

    2.0.0 - assume db has been sublevelified

--- index.js
+++ index.js
@@ -3,17 +3,16 @@ var crunch = require('voxel-crunch')
 
 module.exports = VoxelLevel
 
-function VoxelLevel(game, readyCB) {
-  if (!(this instanceof VoxelLevel)) return new VoxelLevel(game, readyCB)
-  this.game = game
-  this.db = leveljs('blocks')
-  this.db.open(readyCB)
+function VoxelLevel(db) {
+  if (!(this instanceof VoxelLevel)) return new VoxelLevel(db)
+  this.db = db
+  return true
 }
 
-VoxelLevel.prototype.load = function(prefix, chunkPosition, dimensions, cb) {
+VoxelLevel.prototype.load = function(worldName, chunkPosition, dimensions, cb) {
   var chunkLength = dimensions[0] * dimensions[1] * dimensions[2]
-  var chunkIndex = prefix + '|' + chunkPosition.join('|') + '|' + chunkLength
-  this.db.get(chunkIndex, function(err, rle) {
+  var chunkIndex = chunkPosition.join('|') + '|' + chunkLength
+  this.db.sublevel(worldName).get(chunkIndex, function(err, rle) {
     if (err) return cb(err)
     var voxels = new Uint8Array(chunkLength)
     crunch.decode(rle, voxels)
@@ -21,10 +20,9 @@ VoxelLevel.prototype.load = function(prefix, chunkPosition, dimensions, cb) {
   })
 }
 
-VoxelLevel.prototype.store = function(prefix, chunk, cb) {
+VoxelLevel.prototype.store = function(worldName, chunk, cb) {
   var rle = crunch.encode(chunk.voxels)
-  var key = prefix + '|'
-  key += chunk.position.join('|')
+  var key = chunk.position.join('|')
   key += '|' + chunk.voxels.length
-  this.db.put(key, rle, cb)
+  this.db.sublevel(worldName).put(key, rle, cb)
 }
