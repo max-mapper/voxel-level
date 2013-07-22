7ac56e124000d58cd6f71d5d2e56dccd841ecee3
Max Ogden <max@maxogden.com>
2013-07-22 16:10:45 -0700

    2.0.1 - always use binary

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function VoxelLevel(db) {
 VoxelLevel.prototype.load = function(worldName, chunkPosition, dimensions, cb) {
   var chunkLength = dimensions[0] * dimensions[1] * dimensions[2]
   var chunkIndex = chunkPosition.join('|') + '|' + chunkLength
-  this.db.sublevel(worldName).get(chunkIndex, function(err, rle) {
+  this.db.sublevel(worldName).get(chunkIndex, { valueEncoding: 'binary' }, function(err, rle) {
     if (err) return cb(err)
     var voxels = new Uint8Array(chunkLength)
     crunch.decode(rle, voxels)
@@ -24,5 +24,5 @@ VoxelLevel.prototype.store = function(worldName, chunk, cb) {
   var rle = crunch.encode(chunk.voxels)
   var key = chunk.position.join('|')
   key += '|' + chunk.voxels.length
-  this.db.sublevel(worldName).put(key, rle, cb)
+  this.db.sublevel(worldName).put(key, rle, { valueEncoding: 'binary' }, cb)
 }
